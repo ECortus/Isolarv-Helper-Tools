3162031b0e24523185c6e7099416379c362a6a9f
ECortus <ve.cortus@gmail.com>
2025-11-23 22:45:21 +0200

    add downstate button

---
+++ Assets/IsolarvHelperTools/Runtime/UI/DownstateButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace IsolarvHelperTools.Runtime.UI
+{
+    public class DownstateButton : Button
+    {
+        public UnityEvent onHold;
+        public UnityEvent onUp;
+
+        bool isDown = false;
+        
+        void Update()
+        {
+            if (IsPressed())
+            {
+                if (!isDown)
+                {
+                    isDown = true;
+                }   
+                
+                WhilePressed();
+            }
+            else if (!IsPressed() && isDown)
+            {
+                isDown = false;
+                onUp?.Invoke();
+            }
+        }
+        
+        void WhilePressed()
+        {
+            onHold?.Invoke();
+        }
+    }
+
+}
