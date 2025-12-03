49839e0a89f3f653a1518b6d45c33ace148222da
ECortus <ve.cortus@gmail.com>
2025-12-03 16:39:29 +0200

    upd

--- Assets/Plugins/GameDevUtils/Runtime/Colliders/ColliderHelper.cs
+++ Assets/Plugins/GameDevUtils/Runtime/Colliders/ColliderHelper.cs
@@ -6,6 +6,21 @@ namespace GameDevUtils.Runtime.Colliders
     {
         public static bool IsSameMask(this Collider collider, string name)
         {
+            return IsSameMask(collider.gameObject, name);
+        }
+        
+        public static bool IsSameMaskBitwise(this Collider collider, string name)
+        {
+            return IsSameMaskBitwise(collider.gameObject, name);
+        }
+
+        public static bool IsSameMask(this GameObject collider, string name)
+        {
+            return collider.gameObject.layer == LayerMask.NameToLayer(name);
+        }
+        
+        public static bool IsSameMaskBitwise(this GameObject collider, string name)
+        {
             return collider.gameObject.layer == 1 << LayerMask.NameToLayer(name);
         }
     }
