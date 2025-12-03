822e8270450a9f86df82117e9f54e2a5aff00c8e
ECortus <ve.cortus@gmail.com>
2025-12-03 16:15:51 +0200

    upd

---
+++ Assets/Plugins/GameDevUtils/Runtime/Colliders/ColliderHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace GameDevUtils.Runtime.Colliders
+{
+    public static class ColliderHelper
+    {
+        public static bool IsSameMask(this Collider collider, string name)
+        {
+            return collider.gameObject.layer == 1 << LayerMask.NameToLayer(name);
+        }
+    }
+}
