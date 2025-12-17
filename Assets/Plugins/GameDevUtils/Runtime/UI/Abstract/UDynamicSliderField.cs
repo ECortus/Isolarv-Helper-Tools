39e3170e070553b7e45f13bef0761bb8023a10a0
ECortus <ve.cortus@gmail.com>
2025-12-18 00:13:47 +0200

    upd

--- Assets/Plugins/GameDevUtils/Runtime/UI/Abstract/UDynamicSliderField.cs
+++ Assets/Plugins/GameDevUtils/Runtime/UI/Abstract/UDynamicSliderField.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace GameDevUtils.Runtime.UI.Abstract
@@ -7,10 +8,12 @@ namespace GameDevUtils.Runtime.UI.Abstract
     {
         [Space(5)]
         [SerializeField] private Slider slider;
+        [SerializeField] private TMP_Text label;
 
         protected override void UpdateField()
         {
             UpdateSlider();
+            UpdateLabel();
         }
         
         protected abstract float GetSliderValue();
@@ -18,6 +21,13 @@ namespace GameDevUtils.Runtime.UI.Abstract
         void UpdateSlider()
         {
             slider.value = GetSliderValue();
+        }
+        
+        protected virtual string GetLabelText() { return ""; }
+        
+        void UpdateLabel()
+        {
+            label.text = GetLabelText();
         }
     }
 }
