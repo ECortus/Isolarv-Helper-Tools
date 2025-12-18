c7220a4be6118947b04fdd15644ca5628eb9bd54
ECortus <ve.cortus@gmail.com>
2025-12-18 23:47:03 +0200

    upd

---
+++ Assets/Plugins/GameDevUtils/Runtime/Components/AbstractResourceManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameDevUtils.Runtime
+{
+    public abstract class AbstractResourceManager<T> : UnitySingleton<T> where T : MonoBehaviour
+    {
+        float _resource;
+        float Resource
+        {
+            get => _resource;
+            set
+            {
+                if (Mathf.Approximately(value, _resource))
+                {
+                    return;
+                }
+                
+                onChanged?.Invoke();
+                onValueChanged?.Invoke(value);
+                
+                if (value < 0)
+                {
+                    this._resource = 0;
+                    return;
+                }
+                
+                this._resource = value;
+            }
+        }
+        
+        public void Plus(float amount)
+        {
+            Resource += amount;
+        }
+        
+        public void Reduce(float amount)
+        {
+            Resource -= amount;
+        }
+        
+        public float GetValue()
+        {
+            return Resource;
+        }
+        
+        public float GetValueInt()
+        {
+            return (int)Resource;
+        }
+        
+        public void SetValue(float value)
+        {
+            Resource = value;
+        }
+
+        public void ResetValue()
+        {
+            Resource = 0;
+        }
+        
+        public event Action onChanged;
+        public event Action<float> onValueChanged;
+    }
+}
